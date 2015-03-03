d527453d11ebf3a3025edd1b9ee5a98646f7a5a1
tojba <yougotspam123@gmail.com>
2015-03-03 16:05:15 +0100

    geolocation working

--- www/js/index.js
+++ www/js/index.js
@@ -17,7 +17,7 @@
  * under the License.
  */
 
-var app = {
+ var app = {
     // Application Constructor
     initialize: function() {
         this.bindEvents();
@@ -31,24 +31,24 @@ var app = {
         document.addEventListener('deviceready', this.onDeviceReady, false);
     },
 
-    getCurrentLocation: function() {
-        navigator.geolocation.getCurrentPosition(onSuccess, onError);
+    onError: function(error) {
+        alert('code: '    + error.code    + '\n' +
+          'message: ' + error.message + '\n');
     },
     onSuccess: function(position) {
         alert('Latitude: '          + position.coords.latitude          + '\n' +
-              'Longitude: '         + position.coords.longitude         + '\n' +
-              'Altitude: '          + position.coords.altitude          + '\n' +
-              'Accuracy: '          + position.coords.accuracy          + '\n' +
-              'Altitude Accuracy: ' + position.coords.altitudeAccuracy  + '\n' +
-              'Heading: '           + position.coords.heading           + '\n' +
-              'Speed: '             + position.coords.speed             + '\n' +
-              'Timestamp: '         + position.timestamp                + '\n');
+          'Longitude: '         + position.coords.longitude         + '\n' +
+          'Altitude: '          + position.coords.altitude          + '\n' +
+          'Accuracy: '          + position.coords.accuracy          + '\n' +
+          'Altitude Accuracy: ' + position.coords.altitudeAccuracy  + '\n' +
+          'Heading: '           + position.coords.heading           + '\n' +
+          'Speed: '             + position.coords.speed             + '\n' +
+          'Timestamp: '         + position.timestamp                + '\n');
     },
-    onError: function(error) {
-        alert('code: '    + error.code    + '\n' +
-              'message: ' + error.message + '\n');
+    getCurrentLocation: function() {
+        navigator.geolocation.getCurrentPosition(this.onSuccess, this.onError);
     },
-    
+
     // deviceready Event Handler
     //
     // The scope of 'this' is the event. In order to call the 'receivedEvent'
