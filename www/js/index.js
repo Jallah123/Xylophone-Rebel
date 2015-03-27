41cf9bbe956cc98d8109753fb831bd4aeee69061
Tojba <yougotspam123@gmail.com>
2015-03-27 17:55:32 +0100

    added settings and shake and localdb

--- www/js/index.js
+++ www/js/index.js
@@ -22,10 +22,15 @@
     var shownVenues = undefined;
     var currentDetailVenue = undefined;
     var eetNu = undefined;
-
-    $(".footer_text").text("Door mij");
+    var shakeEvent = undefined;
+    var db = undefined;
 
     self.initialize = function(eetNu) {
+        createDb();
+        myShakeEvent = new Shake({
+            threshold: 15 // optional shake strength threshold
+        });
+        myShakeEvent.start();
         self.eetNu = eetNu;
         bindEvents();
         $('#listview').delegate('li', 'tap', function () {
@@ -33,14 +38,33 @@
             currentDetailVenue = shownVenues[index];
             eetNu.getReviewsByVenueId(shownVenues[index]);
         });
-        $("#detail").live("swipeleft", function() {
-            alert("swipe");
+        $("#savesettings").on('click', function(){
+            saveSettings();
         });
+    };
+    createDb = function() {
+        alert("creating");
+        db = openDatabase('mydb', '1.0', 'my first database', 2 * 1024 * 1024);
+        db.transaction(function (tx) {
+          tx.executeSql('CREATE TABLE settings (id unique, max_distance),');
+          tx.executeSql('SELECT * FROM settings', [], function (tx, results) {
+              var len = results.rows.length, i;
+              for (i = 0; i < len; i++) {
+                alert(results.rows.item(i).text);
+            }
+        });
+      };
+      saveSettings = function() {
+        alert($("#max_distance").val());
     };
     bindEvents = function() {
         console.log('bindEvents');
         document.addEventListener('deviceready', onDeviceReady, false);
+        window.addEventListener('shake', shakeEventDidOccur, false);
     };
+    shakeEventDidOccur = function() {
+        $.mobile.changePage("#settings");
+    }
     onError = function(error) {
         alert('code: '    + error.code    + '\n' +
           'message: ' + error.message + '\n');
@@ -97,83 +121,83 @@
         $("#detail").find("#image").attr("src", "");
         $("#detail").find("#title").text(currentDetailVenue.name);
         $("#detail").find("#numberReviews").text(reviews.results.length);
-        
+
         setReviews(reviews);
-        
+
         if(currentDetailVenue.images.original.length > 0){
-             $("#detail").find("#image").attr("src", currentDetailVenue.images.original[0]);
-        }
-        $("#detail").find("#contact").html("Contact \n" + "Telephone: <a href='tel:" + currentDetailVenue.telephone +"'>" + currentDetailVenue.telephone  +  "</a>\n Website: <a id='website_url' href='" + currentDetailVenue.website_url + "' onclick='return app.openExternal();' target='_system'>" + currentDetailVenue.website_url + "</a>");
-        $("#detail").find("#navbutton").attr("onclick","window.open(geo:" + currentDetailVenue.geolocation.latitude + "," + currentDetailVenue.geolocation.longitude + ")");
-        //<button onclick="window.open(" geo:52.0277951,5.0816377')'="" id="navbutton" class=" ui-btn ui-shadow ui-corner-all">Start navigation</button>
-        location.hash = "detail";
-    };
-    function setReviews(reviews){
-        if(reviews.results.length > 0)
-        {
-            var averages = calculateAverageScore(reviews.results);
-        }   
-        if(averages != undefined){
-            $("#detail").find("#food").text(((isNaN(averages['food'])) ? "0" : averages['food']));
-            $("#detail").find("#ambiance").text(((isNaN(averages['ambiance'])) ? "0" : averages['ambiance']));
-            $("#detail").find("#service").text(((isNaN(averages['service'])) ? "0" : averages['service']));
-            $("#detail").find("#value").text(((isNaN(averages['value'])) ? "0" : averages['value']));
-        }else{
-            $("#detail").find("#food").text(0);
-            $("#detail").find("#ambiance").text(0);
-            $("#detail").find("#service").text(0);
-            $("#detail").find("#value").text(0);
-        }
-    };
-    calculateAverageScore = function(reviews) {
-        var averageFood = 0;
-        var averageAmbiance = 0;
-        var averageService = 0;
-        var averageValue = 0;
+         $("#detail").find("#image").attr("src", currentDetailVenue.images.original[0]);
+     }
+     $("#detail").find("#contact").html("Contact \n" + "Telephone: <a href='tel:" + currentDetailVenue.telephone +"'>" + currentDetailVenue.telephone  +  "</a>\n Website: <a id='website_url' href='" + currentDetailVenue.website_url + "' onclick='return app.openExternal();' target='_system'>" + currentDetailVenue.website_url + "</a>");
+     $("#detail").find("#navbutton").attr("onclick","window.open(geo:" + currentDetailVenue.geolocation.latitude + "," + currentDetailVenue.geolocation.longitude + ")");
+            //<button onclick="window.open(" geo:52.0277951,5.0816377')'="" id="navbutton" class=" ui-btn ui-shadow ui-corner-all">Start navigation</button>
+            location.hash = "detail";
+        };
+        function setReviews(reviews){
+            if(reviews.results.length > 0)
+            {
+                var averages = calculateAverageScore(reviews.results);
+            }   
+            if(averages != undefined){
+                $("#detail").find("#food").text(((isNaN(averages['food'])) ? "0" : averages['food']));
+                $("#detail").find("#ambiance").text(((isNaN(averages['ambiance'])) ? "0" : averages['ambiance']));
+                $("#detail").find("#service").text(((isNaN(averages['service'])) ? "0" : averages['service']));
+                $("#detail").find("#value").text(((isNaN(averages['value'])) ? "0" : averages['value']));
+            }else{
+                $("#detail").find("#food").text(0);
+                $("#detail").find("#ambiance").text(0);
+                $("#detail").find("#service").text(0);
+                $("#detail").find("#value").text(0);
+            }
+        };
+        calculateAverageScore = function(reviews) {
+            var averageFood = 0;
+            var averageAmbiance = 0;
+            var averageService = 0;
+            var averageValue = 0;
 
-        var foodCount = 0;
-        var ambianceCount = 0;
-        var serviceCount = 0;
-        var valueCount = 0;
+            var foodCount = 0;
+            var ambianceCount = 0;
+            var serviceCount = 0;
+            var valueCount = 0;
 
-        for(var i = 0; i < reviews.length; i++){
-            if(reviews[i].scores.food != null){
-                averageFood += reviews[i].scores.food;
-                foodCount++;
+            for(var i = 0; i < reviews.length; i++){
+                if(reviews[i].scores.food != null){
+                    averageFood += reviews[i].scores.food;
+                    foodCount++;
+                }
+                if(reviews[i].scores.ambiance != null){
+                    averageAmbiance += reviews[i].scores.ambiance;
+                    ambianceCount++;
+                }
+                if(reviews[i].scores.service != null){
+                    averageService += reviews[i].scores.service;
+                    serviceCount++;
+                }
+                if(reviews[i].scores.value != null){
+                    averageValue += reviews[i].scores.value;
+                    valueCount++;
+                }
             }
-            if(reviews[i].scores.ambiance != null){
-                averageAmbiance += reviews[i].scores.ambiance;
-                ambianceCount++;
-            }
-            if(reviews[i].scores.service != null){
-                averageService += reviews[i].scores.service;
-                serviceCount++;
-            }
-            if(reviews[i].scores.value != null){
-                averageValue += reviews[i].scores.value;
-                valueCount++;
-            }
-        }
 
-        var array = new Array();
-        array['food'] = Math.round((averageFood / foodCount));
-        array['ambiance'] = Math.round((averageAmbiance / ambianceCount));
-        array['service'] = Math.round((averageService / serviceCount));
-        array['value'] = Math.round((averageValue / valueCount));
-        
-        if(foodCount == 0){
-            array['food'] = "No food reviews yet.";
-        }
-        if(ambianceCount == 0){
-            array['ambiance'] = "No ambiance reviews yet.";
-        }
-        if(serviceCount == 0){
-            array['service'] = "No service reviews yet.";
-        }
-        if(valueCount == 0){
-            array['value'] = "No value reviews yet.";
-        }
-        return array;
+            var array = new Array();
+            array['food'] = Math.round((averageFood / foodCount));
+            array['ambiance'] = Math.round((averageAmbiance / ambianceCount));
+            array['service'] = Math.round((averageService / serviceCount));
+            array['value'] = Math.round((averageValue / valueCount));
+            
+            if(foodCount == 0){
+                array['food'] = "No food reviews yet.";
+            }
+            if(ambianceCount == 0){
+                array['ambiance'] = "No ambiance reviews yet.";
+            }
+            if(serviceCount == 0){
+                array['service'] = "No service reviews yet.";
+            }
+            if(valueCount == 0){
+                array['value'] = "No value reviews yet.";
+            }
+            return array;
+        };
+        return self;
     };
-    return self;
-};
